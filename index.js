24776bea23e4778091d60ad6704b5cbe362caf4d
afcLadeira <32864936+afcLadeira@users.noreply.github.com>
2022-06-28 18:19:40 +0100

    cors test

--- index.js
+++ index.js
@@ -34,23 +34,24 @@ app.use(logger)
 
 const credentials = (req,res,next) => {
   const origin = req.headers.origin
-  if (allowedOrigins.includes(origin)) {
+ // if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Credentials' , true)
-  }
+  //}
   next()
 }
 app.use(credentials)
 
 const corsOptions = {
   origin: (origin , callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    //if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null , true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    //} else {
+    //  callback(new Error('!!!Not allowed by CORS'))
+   // }
   },
   optionsSuccessStatus:200
 }
+//app.use(cors(corsOptions))
 app.use(cors(corsOptions))
 
 
